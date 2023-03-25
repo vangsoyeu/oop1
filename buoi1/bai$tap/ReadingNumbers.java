5f784d2975b2fd806120fc69697d26cf39c6e224
phamhoainam <114454365+vangsoyeu@users.noreply.github.com>
2023-03-25 23:38:31 +0700

    bai tap buoi 1

---
+++ buoi1/bai$tap/ReadingNumbers.java
@@ -0,0 +1,7 @@
+package buoi1.bai$tap;
+
+public class ReadingNumbers {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ buoi1/bai$tap/XinChao.java
@@ -0,0 +1,7 @@
+package buoi1.bai$tap;
+public class XinChao {
+    public static void main(String[] args) {
+        String name = "Nam";
+        System.out.println("Hello " + name);
+    }
+}
